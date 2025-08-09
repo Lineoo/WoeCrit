2342c8fbae603770f67051de04ded1f579bde871
Lineoo <dudu13258@163.com>
2025-08-09 19:35:36 +0800

    Prevent pending update influencing world

--- src/client/java/org/linn/woecrit/client/freecam/FreecamInteractionManager.java
+++ src/client/java/org/linn/woecrit/client/freecam/FreecamInteractionManager.java
@@ -1,11 +1,15 @@
 package org.linn.woecrit.client.freecam;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.GameMode;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,5 +34,22 @@ public class FreecamInteractionManager extends ClientPlayerInteractionManager {
     @Override
     public ActionResult interactBlock(ClientPlayerEntity player, Hand hand, BlockHitResult hitResult) {
         return super.interactBlock(player, hand, hitResult);
+    }
+
+    @Override
+    public boolean breakBlock(BlockPos pos) {
+        return super.breakBlock(pos);
+    }
+
+    @Override
+    public boolean attackBlock(BlockPos pos, Direction direction) {
+        if (!this.client.world.getWorldBorder().contains(pos)) {
+            return false;
+        }
+
+        this.breakBlock(pos);
+        // FIXME this.blockBreakingCooldown = 5;
+
+        return true;
     }
 }
